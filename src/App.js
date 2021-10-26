2aa8931c9cec49d3fcfe851dbc8d62b6fb6cbf6c
Nông Hải <68391535+hainh777@users.noreply.github.com>
2021-10-26 17:27:49 +0700

    learn Context API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import TrafficLight from './components/TrafficLight.js';
 import Counter from './components/Counter.js';
 import { RED, GREEN, ORANGE } from './AppConstants.js'
 
+import NumberProvider from './components/NumberProvider.js'
+import NumberContext from "./context/NumberContext"
+
 class App extends React.Component {
   constructor() {
     super();
@@ -32,22 +35,39 @@ class App extends React.Component {
     }
   };
 
+
   render() {
     return (
-      <div className="App">
-        <TrafficLight currentColor={this.state.currentColor} />
-        <Counter>
-          {
-            function ({ counter, hour, minute }) {
-              return (
-                <div>
-                  <h1>{hour}:{minute}:{counter}</h1>
-                </div>
-              )
+      <NumberProvider>
+        <div className="App">
+          <TrafficLight currentColor={this.state.currentColor} />
+          <Counter>
+            {
+              function ({ counter, hour, minute }) {
+                return (
+                  <div>
+                    <h1>{hour}:{minute}:{counter}</h1>
+                  </div>
+                )
+              }
             }
-          }
-        </Counter>
-      </div>
+          </Counter>
+          <a href="tel:0964437879" >thí í a</a>
+
+          <NumberContext.Consumer>
+            {
+              function ({ number, updateNumber }) {
+                return (
+                  <div>
+                    <h1>{number}</h1>
+                    <button onClick={updateNumber}>update number</button>
+                  </div>
+                )
+              }
+            }
+          </NumberContext.Consumer>
+        </div>
+      </NumberProvider>
     );
   }
 }
